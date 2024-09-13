fab02b6996e5e7e836bac1308d4f6c4d1d382205
Shreya <128571799+shreyakulal55@users.noreply.github.com>
2024-09-13 12:26:40 +0530

    Add files via upload

---
+++ 21AI72/ANN.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]  # petal length, petal width
+y = (iris.target == 0).astype(int)  # Iris Setosa?
+
+per_clf = Perceptron(random_state=42)
+per_clf.fit(X, y)
+y_pred = per_clf.predict([[2, 0.5]])
+print(y_pred)
